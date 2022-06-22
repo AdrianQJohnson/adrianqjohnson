f9307604ae9af6aada65c3d8d810085bb0d7d3f6
Adrian Johnson <adrianqjohnson@gmail.com>
2022-06-21 21:59:27 -0400

    Created todo component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,11 +6,13 @@ import { ContactComponent } from './pages/contact/contact.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ProjectComponent } from './pages/project/project.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
+import { TodoComponent } from './pages/todo/todo.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, data: {title: 'Home | Adrian Johnson'} },
   { path: 'about', component: AboutComponent, data: {title: 'About Me | Adrian Johnson'} },
   { path: 'contact', component: ContactComponent, data: {title: 'Contact Me | Adrian Johnson'} },
+  { path: 'todo', component: TodoComponent, data: {title: 'Personal Assignments | Adrian Johnson'} },
   { path: 'projects', component: ProjectsComponent, data: {title: 'All Projects | Adrian Johnson'} },
   { path: 'projects/:category/:project', component: ProjectComponent, data: {title: 'Project | Adrian Johnson'} },
 
